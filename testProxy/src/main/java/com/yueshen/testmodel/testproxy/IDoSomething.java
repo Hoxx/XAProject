b0e3ff274d5e069208292deb6438f2dd7e503e69
Hxx-Company <1416942919@qq.com>
2018-06-05 16:19:14 +0800

    修改代理代码

--- testProxy/src/main/java/com/yueshen/testmodel/testproxy/IDoSomething.java
+++
@@ -1,8 +0,0 @@
-package com.yueshen.testmodel.testproxy;
-
-//接口
-public interface IDoSomething {
-
-    int doSomething(int sum);
-
-}
--- testProxy/src/main/java/com/yueshen/testmodel/testproxy/IProxyClass.java
+++
@@ -1,7 +0,0 @@
-package com.yueshen.testmodel.testproxy;
-
-public interface IProxyClass {
-    int doSomething(int i);
-
-    String addFunction(String msg);
-}
